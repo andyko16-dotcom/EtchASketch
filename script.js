c89718c3cac0f5561d69707be7404190f9c1e117
andyko16-dotcom <andyko16@gmail.com>
2023-04-14 22:11:29 -0700

    finished yet not finished

--- script.js
+++ script.js
@@ -1,76 +1,85 @@
 //creating 16x16 div box with help of flex
 
 const container = document.querySelector('#drawBox');
+const resizeButton = document.querySelector('.resize');
 
-for (let i = 0; i <=15; i++) {
-    let column = document.createElement('div');
-    column.classList.add('column');
-
-
-
-for (let i =0; i<16; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    column.appendChild(cell);
+function createCells (numCells) {
+    container.innerHTML = '' //clear existing cell
+    container.style.gridTemplateRows = `repeat(${numCells}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${numCells}, 1fr)`;
 
 
-}
 
-container.appendChild(column);
-}
+    for(let i = 0; i < numCells*numCells; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.style.backgroundColor = 'rgb(255,255,255)';
+        container.appendChild(cell);
+    }
 
 
-///mousedown, mousemove, mouseup
+    //for addeventlistener mouseup mousedown mousemove
+    let isDrawing = false;
+    let clicked = false;
+    const cells = document.querySelectorAll('.cell');
 
-let isDrawing = false;
-
-//changing color of cells when pressed
-
-const cells = document.querySelectorAll('.cell');
-
-let cellColor
-
-cells.forEach(cell => {
-    cell.addEventListener('click', function() {
-        if (shade) {
-            let newRgb = shades(cell.style.backgroundColor)
-            cell.style.backgroundColor = newRgb
+    cells.forEach(cell => {
+        cell.addEventListener('click', function() {
+            if (shade) {
+                let newRgb = shades(cell.style.backgroundColor);
+                cell.style.backgroundColor = newRgb;
+                
+            } else if (rainbow) {
+                let ranColor = randomColor();
+                cell.style.backgroundColor = ranColor;
+            } else {
+                cell.style.backgroundColor = cellColor;
+            }
+    
+    
+        })
+    
+        cell.addEventListener('mousedown', () => {
+            isDrawing = true;
+            clicked = true;
             
-        } else {
-            cell.style.backgroundColor = cellColor;
-        }
-
-
-    })
-
-    cell.addEventListener('mousedown', () => {
-        isDrawing = true;
-        shade = true;
-    })
-    
-    cell.addEventListener('mouseup', () => {
-        isDrawing = false;
-        shade = false;
-    })
-    
-    cell.addEventListener('mousemove', (e) => {
-        if (shade) {
-            const cell = e.target;
-            cell.style.backgroundColor = shades(cell.style.backgroundColor)
-    
-        }
-        if (isDrawing) {
-            const cell = e.target;
-            cell.style.backgroundColor = cellColor;
-
-        }
+        })
         
+        cell.addEventListener('mouseup', () => {
+            isDrawing = false;
+            clicked = false;
+            
+            
+        })
+        
+        cell.addEventListener('mousemove', function(e) {
+            const cell = e.target;
+
+            if (shade && clicked) {
+                cell.style.backgroundColor = shades(cell.style.backgroundColor);
+                
+            } else if (rainbow && clicked) {
+                cell.style.backgroundColor = randomColor(cell.style.backgroundColor);
+                
+            } else if (isDrawing && clicked) {
+                cell.style.backgroundColor = cellColor;
+            }
+
+
+        })
     })
 
+}
 
-
-})
-
+resizeButton.addEventListener('click', function() {
+    const numCells = prompt('Enter grid size');
+    if (numCells > 1 && numCells < 65) {
+        createCells(numCells)
+        
+    } else {
+        alert('Enter a number between 2 - 50');
+    }
+}); 
 
 //this is for preset colors and eraser
 const colors = document.querySelectorAll('button');
@@ -80,6 +89,7 @@ colors.forEach(color => {
         switch (this.classList.value) {
             case 'red':
                 cellColor = 'rgb(255, 0, 0)';
+                console.log('red')
                 break
             case 'blue':
                 cellColor = 'rgb(0,0,255)';
@@ -109,7 +119,7 @@ colors.forEach(color => {
 
 //for input type color
 const colorPicker = document.querySelector('.colorPicker')
-
+let cellColor;
 colorPicker.addEventListener('change', function() {
     cellColor = this.value;
 });
@@ -120,6 +130,7 @@ const shaderOnOff = document.querySelector('.shader');
 let shade = false;
 
 shaderOnOff.addEventListener('click', function(e) {
+    shaderOnOff.classList.toggle('dark');
     shade = !shade;
 });
 
@@ -135,9 +146,9 @@ function shades(color) {
     let b = parseInt(selectedColor[1]);
     let g = parseInt(selectedColor[2]);
 
-    let newR = Math.max(0, r - 1);
-    let newB = Math.max(0, b - 1);
-    let newG = Math.max(0, g - 1);
+    let newR = Math.max(0, r - 2);
+    let newB = Math.max(0, b - 2);
+    let newG = Math.max(0, g - 2);
     
     return`rgb(${newR}, ${newB}, ${newG})`
 
@@ -145,3 +156,36 @@ function shades(color) {
 }
 
 
+
+//randomColor function
+function randomColor () {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+
+    let rainbowColor = `rgb(${red}, ${green}, ${blue})`
+
+    return rainbowColor
+}
+
+
+//randomColor Control Panel
+const rainbowOnOff = document.querySelector('.rainbow');
+let rainbow = false;
+
+rainbowOnOff.addEventListener('click', function() {
+    rainbowOnOff.classList.toggle('dark');
+    rainbow = !rainbow;
+});
+
+
+
+
+
+
+
+
+
+//inital
+
+createCells(16); // default size of cells
